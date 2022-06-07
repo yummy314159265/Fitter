6e853b60d3a8b71295f31d9800e89401151ef750
Alex Blanke <alexblanke98@gmail.com>
2022-06-07 11:59:39 -0500

    Profile page

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Start from './pages/Start';
 import Footer from './components/Footer';
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Posts from './pages/Timeline'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Posts from './pages/Timeline';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -36,17 +37,14 @@ function App() {
             path="/signup" 
             element={<Signup />} 
           />
-<<<<<<< HEAD
           <Route
             path="/profile"
             element={<Profile />}
           />
-=======
           <Route 
             path="/posts"
             element={<Posts />}
             />
->>>>>>> 2b98bd46179b240fdcfd61d52eb42a436de096a1
         </Routes>
         <Footer />
       </Router>
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -16,21 +16,11 @@ import {
     Icon,
     useColorModeValue,
   } from '@chakra-ui/react';
-//   import {
-//     IoAnalyticsSharp,
-//     IoLogoBitcoin,
-//     IoSearchSharp,
-//   } from 'react-icons';
 import { Button, ButtonProps} from '@chakra-ui/react';
 import { useState } from 'react';
-  import { ReactElement } from 'react';
-  
-//   interface FeatureProps {
-//     text: string;
-//     iconBg: string;
-//     icon?: ReactElement;
-//   }
-  
+import { ReactElement } from 'react';
+
+
   const Feature = ({ text, icon, iconBg }) => {
     return (
       <Stack direction={'row'} align={'center'}>
@@ -69,7 +59,7 @@ import { useState } from 'react';
 
             <Heading>USERNAME'S Profile</Heading>
             <Text color={'gray.500'} fontSize={'lg'}>
-              ALL USER DATA: \n
+              ALL USER DATA:
               WEIGHT
               HEIGHT
               AGE
@@ -78,6 +68,18 @@ import { useState } from 'react';
               EXERCISE PLAN
               MEAL PLAN
             </Text>
+                <Button
+                    px={8}
+                    bg={useColorModeValue('#151f21', 'gray.900')}
+                    color={'white'}
+                    rounded={'md'}
+                    _hover={{
+                        transform: 'translateY(-2px)',
+                        boxShadow: 'lg',
+                    }}
+                    onClick={console.log("poke")}>
+                    Create Post
+                </Button>
             <Stack
               spacing={4}
               divider={
@@ -86,26 +88,41 @@ import { useState } from 'react';
                 />
               }>
               <Feature
-                // icon={
-                //   <Icon as={IoAnalyticsSharp} color={'yellow.500'} w={5} h={5} />
-                // }
                 iconBg={useColorModeValue('yellow.100', 'yellow.900')}
-                text={'Business Planning'}
+                text={'Your current goal : '}
+                // add goal
               />
               <Feature
-                // icon={<Icon as={IoLogoBitcoin} color={'green.500'} w={5} h={5} />}
                 iconBg={useColorModeValue('green.100', 'green.900')}
-                text={'Financial Planning'}
+                text={'Your current exercise plan : '}
+                // add goal
               />
               <Feature
-                // icon={
-                //   <Icon as={IoSearchSharp} color={'purple.500'} w={5} h={5} />
-                // }
                 iconBg={useColorModeValue('purple.100', 'purple.900')}
-                text={'Market Analysis'}
+                text={'Your current meal plan : '}
+                // add meal plan
+              />
+              <Feature
+                iconBg={useColorModeValue('red.100', 'red.900')}
+                text={'Your most recent post : '}
+                // add post
               />
             </Stack>
-            <Stack
+
+          </Stack>
+          <Flex>
+            <Image
+            // maybe some kind of chart? track weight over time or something? or maybe track cheat days/days you did the work?
+              rounded={'md'}
+              alt={'feature image'}
+              src={
+                'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+              }
+              objectFit={'cover'}
+            />
+          </Flex>
+        </SimpleGrid>
+        <Stack
               spacing={4}
               divider={
                 <StackDivider
@@ -148,19 +165,19 @@ import { useState } from 'react';
                     onClick={console.log("poke")}>
                     Create Exercise Plan
                 </Button>
+                <Button
+                    px={8}
+                    bg={useColorModeValue('#151f21', 'gray.900')}
+                    color={'white'}
+                    rounded={'md'}
+                    _hover={{
+                        transform: 'translateY(-2px)',
+                        boxShadow: 'lg',
+                    }}
+                    onClick={console.log("poke")}>
+                    Edit Goal
+                </Button>
             </Stack>
-          </Stack>
-          <Flex>
-            <Image
-              rounded={'md'}
-              alt={'feature image'}
-              src={
-                'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-              }
-              objectFit={'cover'}
-            />
-          </Flex>
-        </SimpleGrid>
       </Container>
     );
   }
