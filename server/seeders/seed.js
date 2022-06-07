82d692d719fbafd56a33df13a7566ab22f3fa2b1
Mayur Patel <pmayur0680@gmail.com>
2022-06-07 10:58:08 -0500

    using casual library for seed file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,8 +1,20 @@
-const connection = require('../config/connection');
-const { User } = require('../models');
-connection.on('error', (err) => err);
+var casual = require('casual');
 
-connection.once('open', async () => {
-    console.log('connected');
+const users = [];
+for (let i = 0; i < 20; i++) {
+    
+    users.push({
+      casual.username,
+      last,
+      age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
+    });
+}
+console.log(casual.username);
+// const connection = require('../config/connection');
+// const { User } = require('../models');
+// connection.on('error', (err) => err);
+
+// connection.once('open', async () => {
+//     console.log('connected');
    
-});
+// });
