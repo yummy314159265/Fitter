ad2e62dbac25b3e01398de22b309c5b7a142e93a
Mayur Patel <pmayur0680@gmail.com>
2022-06-07 12:22:14 -0500

    adduser mutation and seed file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,15 +1,19 @@
 var casual = require('casual');
 const connection = require('../config/connection');
 const { User } = require('../models');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 connection.on('error', (err) => err);
 
 const users = [];
 const gender = ['M','F']
 for (let i = 0; i < 20; i++) {
-    
+    let salt = bcrypt.genSaltSync(saltRounds);
+    let hash = bcrypt.hashSync(casual.password, salt);
     users.push({
       "username": casual.username,
-      "password": casual.password,
+      "password": hash,
       "email": casual.email,
       "private": casual.boolean,
       "weight": Math.floor(Math.random() * 450) + 90,
