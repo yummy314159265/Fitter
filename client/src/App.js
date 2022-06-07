46a8a759ea118c21441d51a90bf5aae796128a8c
Alex Blanke <alexblanke98@gmail.com>
2022-06-07 11:33:59 -0500

    Starting Profile

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Start from './pages/Start';
 import Footer from './components/Footer';
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -35,6 +36,10 @@ function App() {
           <Route 
             path="/signup" 
             element={<Signup />} 
+          />
+          <Route
+            path="/profile"
+            element={<Profile />}
           />
         </Routes>
         <Footer />
