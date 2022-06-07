eff4fbea43a031b26d0d89b65b99bfdbc0e90a88
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2022-06-07 03:57:27 -0500

    added formik hook to signup and logins, added logos

--- client/src/components/Logo/Logo.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Image } from '@chakra-ui/react';
-
-export default function Logo({ img, boxSize }) {
-  return <Image boxSize={boxSize} objectFit='contain' src={img} alt='Logo' />
-}
