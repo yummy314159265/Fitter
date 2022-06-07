78f1d630477163bbafc3c3cc704e743676f1f02f
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2022-06-07 13:39:06 -0500

    adding seeds

--- server/models/Exercise.js
+++ server/models/Exercise.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const exerciseSchema = new Schema({
   name: {
--- server/models/User.js
+++ server/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
   username: {
     type: String,
     required: true,
+    unique: true
   },
   password: {
     type: String,
@@ -14,6 +15,7 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
+    unique: true
   },
   private: {
     type: Boolean,
