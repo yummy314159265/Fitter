a644b12b8bf33efc2598746577ea66004446eb56
Mayur Patel <pmayur0680@gmail.com>
2022-06-10 23:09:49 -0500

    fixed resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,9 +5,8 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
        me: async (parent, args, context) => {
-         if (context.user) {
-           console.log(context.user)
-          //  const userData = await User.findOne({ _id: context.user._id })
+         if (context.user) {           
+           const userData = await User.findOne({ _id: context.user._id })
            .populate('mealPlan')
            .populate('exercisePlan')
            .populate('goals')
