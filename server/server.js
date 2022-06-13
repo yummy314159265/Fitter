422e328eb7496e76112c0216d8f3fc4858e221f6
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2022-06-12 20:25:01 -0500

    fixing heroku issues

--- server/server.js
+++ server/server.js
@@ -9,10 +9,10 @@ const express = require('express');
 
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('server/utils/auth');
+const { authMiddleware } = require('./utils/auth.js');
 
-const { typeDefs, resolvers } = require('server/schemas');
-const db = require('server/config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
